8045154b32c06d62cb02ba930938b606f78ddb09
Brandan Lennox <brandan@bclennox.com>
2015-04-28 22:28:52 -0700

    Optimized for parallelization.

--- primes.go
+++ primes.go
@@ -3,37 +3,25 @@ package main
 import (
   "fmt"
   "os"
+  "runtime"
   "strconv"
 )
 
-func generate(limit int) <-chan int {
-  c := make(chan int)
-  
-  go func() {
-    c <- 2
-    for i := 3; i <= limit; i += 2 {
-      c <- i
-    }
-    close(c)
-  }()
-  
-  return c
+func generate(limit int, g chan<- int, q chan<- bool) {
+  g <- 2
+  for i := 3; i <= limit; i += 2 {
+    g <- i
+  }
+  q <- true
 }
 
-func primes(g <-chan int) <-chan int {
-  p := make(chan int)
-  
-  go func() {
-    for n := range g {
-      check(n, p)
-    }
-    close(p)
-  }()
-  
-  return p
+func primes(g <-chan int, p chan<- int) {
+  for n := range g {
+    check(n, p)
+  }
 }
 
-func check(n int, p chan int){
+func check(n int, p chan<- int){
   for i := 2; i <= n / 2; i++ {
     if n % i == 0 {
       return
@@ -47,9 +35,26 @@ func output(p int){
 }
 
 func main() {
-  limit, _ := strconv.Atoi(os.Args[1])
+  runtime.GOMAXPROCS(runtime.NumCPU())
 
-  for p := range primes(generate(limit)) {
-    output(p)
+  limit, _ := strconv.Atoi(os.Args[1])
+  g := make(chan int)
+  p := make(chan int)
+  q := make(chan bool)
+
+  go generate(limit, g, q)
+  for i := 0; i < runtime.NumCPU(); i++ {
+    go primes(g, p)
+  }
+
+  go func() {
+    if <-q {
+      close(g)
+      close(p)
+    }
+  }()
+
+  for n := range p {
+    output(n)
   }
 }
